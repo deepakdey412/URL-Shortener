6f007a58eb29f3850eb1db9497698ffed288e8f9
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-01-05 14:22:04 +0530

    Created the controller class

---
+++ url-shortner/src/main/java/com/url_shortner/controller/UrlController.java
@@ -0,0 +1,35 @@
+package com.url_shortner.controller;
+
+import com.url_shortner.entity.UrlMapping;
+import com.url_shortner.service.IUrlService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/url-shortner")
+public class UrlController {
+
+    private final IUrlService  urlService;
+    public UrlController(IUrlService urlService) {
+        this.urlService = urlService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<UrlMapping> create(@RequestParam String url) {
+        UrlMapping shortUrl = urlService.createShortUrl(url);
+        return  new ResponseEntity<>(shortUrl, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{shortCode}")
+    public ResponseEntity<UrlMapping> updateShortUrl(@PathVariable String shortCode , @RequestParam String url) {
+        UrlMapping urlMapping = urlService.updateLongUrl(shortCode, url);
+        return  new ResponseEntity<>(urlMapping, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{shortCode}")
+    public ResponseEntity<String > deleteShortUrl(@PathVariable String shortCode) {
+        String msg = urlService.deleteShortUrl(shortCode);
+        return  new ResponseEntity<>(msg, HttpStatus.OK);
+    }
+}
